004cb79c9a0dfa964a9bbbfc23999b238beb6f38
seedfelho <sergio.filho2@estudante.ifms.edu.br>
2023-08-22 08:16:29 -0400

    comentei o import do footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from './componentes/header/Header'
-import Footer from './componentes/footer/Footer'
+// import Footer from './componentes/footer/Footer'
 import Footer1 from './componentes/footer1/Footer1'
 
 function App() {
