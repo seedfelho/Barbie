7657cf3274b95189289c56981e95fd72b55093f9
seedfelho <sergio.filho2@estudante.ifms.edu.br>
2023-08-31 10:20:29 -0400

    arrumando img

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -6,7 +6,7 @@ function Main(){
         <main className="content-main">
             <Filme titulo= "Barbie" sinopse="Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita,
              Barbie parte para o mundo humano em busca da verdadeira felicidade."
-             imagem="/barbie.png"
+             imagem="/cartazBarbie.png"
              />
             <Filme/>
             <Filme/>
